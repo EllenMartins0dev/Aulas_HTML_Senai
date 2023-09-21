d288fba201629af2ebf9ac41868f5f939f3e79b7
Ellen <ellenmartins@aluno.senai.br>
2023-09-21 11:27:43 -0300

    Enviando arquivos de HTML pro repositório

---
+++ HTML/Aula dia 21.09/AtividadeJS/script.js
@@ -0,0 +1,18 @@
+alert("APROVADOS E LOCAIS DE PROVA!")
+console.log("APROVADOS E LOCAIS DE PROVA")
+console.log(" ")
+console.log(" ")
+
+console.log("Lista de aprovados:")
+let approved = ["Ana Cassia", "Ana Lucia", "Marcelo Alves", "Ana Júlia"]
+approved.forEach(approved => {
+    console.log(approved)
+})
+
+console.log(" ")
+
+console.log("Locais das provas por ordem:")
+let cities = ["Piracicaba", "Santos", "Americana", "Limeira"]
+cities.forEach(cities => {
+    console.log(cities)
+})
